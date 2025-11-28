7358a11143a33bd514bb4d85990396b8404fca16
monaco11m <elmer.chp@gmail.com>
2025-11-28 01:48:29 -0500

    add unit tessts

---
+++ Nubelity.Tests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Nubelity.API.Controllers;
+using Nubelity.Application.Interfaces;
+using System.Text.Json;
+using Xunit;
+
+namespace Nubelity.Tests.Controllers;
+
+public class AuthControllerTests
+{
+    private readonly Mock<IAuthService> _authService = new();
+    [Fact]
+    public async Task Login_ShouldReturnOk_WithToken()
+    {
+        var controller = new AuthController(_authService.Object);
+
+        string fakeToken = "jwt_token_string";
+
+        _authService
+            .Setup(x => x.LoginAsync("elmer", "123"))
+            .ReturnsAsync(fakeToken);
+
+        var result = await controller.Login("elmer", "123");
+
+        result.Should().BeOfType<OkObjectResult>();
+
+        var ok = result as OkObjectResult;
+        ok.Should().NotBeNull();
+
+        var value = ok!.Value; 
+
+        var prop = value!.GetType().GetProperty("token");
+        var returnedToken = prop!.GetValue(value) as string;
+
+        returnedToken.Should().Be(fakeToken);
+    }
+}
---
+++ Nubelity.Tests/Services/BookServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Moq;
+using Nubelity.Application.DTOs.Books;
+using Nubelity.Application.Exceptions;
+using Nubelity.Application.Interfaces;
+using Nubelity.Application.Services;
+using Xunit;
+
+namespace Nubelity.Tests.Services;
+
+public class BookServiceTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWork = new();
+    private readonly Mock<INormalizationService> _normalizer = new();
+    private readonly Mock<IIsbnSoapService> _isbnValidator = new();
+    private readonly Mock<ICoverImageService> _cover = new();
+
+    [Fact]
+    public async Task CreateAsync_ShouldThrow_WhenIsbnIsInvalid()
+    {
+        var service = new BookService(
+            _unitOfWork.Object,
+            _normalizer.Object,
+            _isbnValidator.Object,
+            _cover.Object
+        );
+
+        _isbnValidator
+            .Setup(x => x.ValidateIsbnAsync(It.IsAny<string>()))
+            .ReturnsAsync(false);
+
+        var dto = new BookCreateDto
+        {
+            Title = "Test Title",
+            Isbn = "123456",
+            PageNumber = 100,
+            AuthorId = Guid.NewGuid()
+        };
+
+        Func<Task> act = async () => await service.CreateAsync(dto);
+
+        var ex = await act.Should()
+            .ThrowAsync<DomainValidationException>();
+
+        ex.And.Message.Should().Be("Validation failed");
+        ex.And.Errors.Should().Contain("Invalid ISBN");
+    }
+}
